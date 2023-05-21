13c8967929282dbbd00ab945f3f8556cf261d4d9
addoumi mohammed <addoumi.mohammed@gmail.com>
2023-05-21 13:50:32 +0100

    feat: init project

---
+++ src/main/java/com/commerceplatform/commerceplatformeurekaserver/CommercePlatformEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.commerceplatform.commerceplatformeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercePlatformEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommercePlatformEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commerceplatform/commerceplatformeurekaserver/CommercePlatformEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commerceplatform.commerceplatformeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercePlatformEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
